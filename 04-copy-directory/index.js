b9c02e54c4c065d8e3a6081de57d5fca0fddaa8f
Klim <kkaliuzhy@gmail.com>
2023-05-10 18:19:36 +0300

    feat: add sth in fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,8 +6,31 @@ const fp = require("fs/promises");
 const mypath = path.join(__dirname,"files");
 const copyPath=path.join(__dirname,"files-copy");
 
+
+async function foo1(src) 
+{
+    await fp.mkdir(copyPath,{ recursive: true });
+    const lotdata = await fp.readdir(src, {withFileTypes: true});
+    for (let file of lotdata) 
+    {
+        const initialPath = path.join(src, file.name);
+        if(file.isDirectory())
+        {
+            await foo(initialPath);
+           
+        }
+        else
+        {
+            await fp.unlink(initialPath);
+           
+        }
+    }
+}
+foo1(copyPath);
 async function foo(src,dest) {
     const lotdata = await fp.readdir(src, {withFileTypes: true});
+   
+
     await fp.mkdir(dest,{ recursive: true });
     for(let data of lotdata) {
         const initialPath = path.join(src, data.name);
